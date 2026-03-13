5f8e744e7c7922b9cea43132e839d80f71286d7a
Diffblue CI <ci@diffblue.com>
2026-03-13 12:08:13 +0000

    test(applications/client): add generated tests for com.paritytrading.parity.client.TradesCommand.execute(Lcom/paritytrading/parity/client/TerminalClient;Ljava/util/Scanner;)V

--- applications/client/src/test/java/com/paritytrading/parity/client/TradesCommandTest.java
+++ applications/client/src/test/java/com/paritytrading/parity/client/TradesCommandTest.java
@@ -16,8 +16,21 @@
 package com.paritytrading.parity.client;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.paritytrading.parity.net.poe.POEClientListener;
+import com.paritytrading.parity.util.Instrument;
+import com.paritytrading.parity.util.Instruments;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class TradesCommandTest {
 
@@ -40,5 +53,127 @@ public class TradesCommandTest {
         TradesCommand command = new TradesCommand();
         String name = command.getName();
         assertEquals("trades", name);
+    }
+
+    @Test
+    public void testExecuteWithNoArguments() throws Exception {
+        TradesCommand command = new TradesCommand();
+
+        Events events = new Events();
+        Instruments instruments = createInstruments(10, 8);
+
+        SocketChannel channel = null;
+        Selector selector = null;
+        OrderEntry orderEntry = null;
+
+        try {
+            channel = SocketChannel.open();
+            channel.configureBlocking(false);
+            selector = Selector.open();
+            channel.register(selector, SelectionKey.OP_READ);
+
+            POEClientListener listener = Mockito.mock(POEClientListener.class);
+            orderEntry = createOrderEntry(selector, channel, listener);
+
+            TerminalClient client = createTerminalClient(events, orderEntry, instruments);
+            Scanner scanner = new Scanner("");
+
+            assertDoesNotThrow(() -> command.execute(client, scanner));
+        } finally {
+            if (orderEntry != null) orderEntry.close();
+            if (channel != null && channel.isOpen()) channel.close();
+            if (selector != null && selector.isOpen()) selector.close();
+        }
+    }
+
+    @Test
+    public void testExecuteWithArguments() throws Exception {
+        TradesCommand command = new TradesCommand();
+
+        Events events = new Events();
+        Instruments instruments = createInstruments(10, 8);
+
+        SocketChannel channel = null;
+        Selector selector = null;
+        OrderEntry orderEntry = null;
+
+        try {
+            channel = SocketChannel.open();
+            channel.configureBlocking(false);
+            selector = Selector.open();
+            channel.register(selector, SelectionKey.OP_READ);
+
+            POEClientListener listener = Mockito.mock(POEClientListener.class);
+            orderEntry = createOrderEntry(selector, channel, listener);
+
+            TerminalClient client = createTerminalClient(events, orderEntry, instruments);
+            Scanner scanner = new Scanner("extra argument");
+
+            assertThrows(IllegalArgumentException.class, () -> command.execute(client, scanner));
+        } finally {
+            if (orderEntry != null) orderEntry.close();
+            if (channel != null && channel.isOpen()) channel.close();
+            if (selector != null && selector.isOpen()) selector.close();
+        }
+    }
+
+    @Test
+    public void testExecuteWithTrades() throws Exception {
+        TradesCommand command = new TradesCommand();
+
+        Events events = new Events();
+        Instruments instruments = createInstruments(12, 10);
+
+        SocketChannel channel = null;
+        Selector selector = null;
+        OrderEntry orderEntry = null;
+
+        try {
+            channel = SocketChannel.open();
+            channel.configureBlocking(false);
+            selector = Selector.open();
+            channel.register(selector, SelectionKey.OP_READ);
+
+            POEClientListener listener = Mockito.mock(POEClientListener.class);
+            orderEntry = createOrderEntry(selector, channel, listener);
+
+            TerminalClient client = createTerminalClient(events, orderEntry, instruments);
+            Scanner scanner = new Scanner("");
+
+            assertDoesNotThrow(() -> command.execute(client, scanner));
+        } finally {
+            if (orderEntry != null) orderEntry.close();
+            if (channel != null && channel.isOpen()) channel.close();
+            if (selector != null && selector.isOpen()) selector.close();
+        }
+    }
+
+    private Instruments createInstruments(int priceWidth, int sizeWidth) throws Exception {
+        Constructor<Instruments> constructor = Instruments.class.getDeclaredConstructor(
+            Instrument[].class, int.class, int.class, int.class, int.class);
+        constructor.setAccessible(true);
+
+        Instrument[] instruments = new Instrument[0];
+        int priceIntegerDigits = priceWidth;
+        int maxPriceFractionDigits = 0;
+        int sizeIntegerDigits = sizeWidth;
+        int maxSizeFractionDigits = 0;
+
+        return constructor.newInstance(instruments, priceIntegerDigits, maxPriceFractionDigits,
+            sizeIntegerDigits, maxSizeFractionDigits);
+    }
+
+    private OrderEntry createOrderEntry(Selector selector, SocketChannel channel, POEClientListener listener) throws Exception {
+        Constructor<OrderEntry> constructor = OrderEntry.class.getDeclaredConstructor(
+            Selector.class, SocketChannel.class, POEClientListener.class);
+        constructor.setAccessible(true);
+        return constructor.newInstance(selector, channel, listener);
+    }
+
+    private TerminalClient createTerminalClient(Events events, OrderEntry orderEntry, Instruments instruments) throws Exception {
+        Constructor<TerminalClient> constructor = TerminalClient.class.getDeclaredConstructor(
+            Events.class, OrderEntry.class, Instruments.class);
+        constructor.setAccessible(true);
+        return constructor.newInstance(events, orderEntry, instruments);
     }
 }
