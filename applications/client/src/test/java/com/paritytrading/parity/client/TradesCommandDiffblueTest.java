a20dbe52ff7bd4e51adca6492aef0ecc1e607219
Diffblue CI <ci@diffblue.com>
2026-04-01 02:59:22 +0000

    test(applications/client): Add generated tests for com.paritytrading.parity.client.TradesCommand.execute(Lcom/paritytrading/parity/client/TerminalClient;Ljava/util/Scanner;)V

--- applications/client/src/test/java/com/paritytrading/parity/client/TradesCommandDiffblueTest.java
+++ applications/client/src/test/java/com/paritytrading/parity/client/TradesCommandDiffblueTest.java
@@ -2,8 +2,12 @@ package com.paritytrading.parity.client;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import com.diffblue.cover.annotations.ManagedByDiffblue;
 import com.diffblue.cover.annotations.MethodsUnderTest;
+import com.paritytrading.parity.util.Instruments;
 import java.util.Scanner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -33,6 +37,40 @@ class TradesCommandDiffblueTest {
     // Act and Assert
     assertThrows(
         IllegalArgumentException.class, () -> tradesCommand.execute(null, new Scanner("foo")));
+  }
+
+  /**
+   * Test {@link TradesCommand#execute(TerminalClient, Scanner)}.
+   *
+   * <ul>
+   *   <li>When {@link Scanner#Scanner(String)} with empty string.
+   *   <li>Then invoke {@link TerminalClient#getInstruments()} and {@link TerminalClient#getEvents()}.
+   * </ul>
+   *
+   * <p>Method under test: {@link TradesCommand#execute(TerminalClient, Scanner)}
+   */
+  @Test
+  @DisplayName(
+      "Test execute(TerminalClient, Scanner); when Scanner(String) with empty string; then invoke getInstruments and getEvents")
+  @Tag("ContributionFromDiffblue")
+  @ManagedByDiffblue
+  @MethodsUnderTest({"void TradesCommand.execute(TerminalClient, Scanner)"})
+  void testExecute_whenScannerEmpty_thenInvokeGetInstrumentsAndGetEvents() {
+    // Arrange
+    TradesCommand tradesCommand = new TradesCommand();
+    TerminalClient client = mock(TerminalClient.class);
+    Instruments instruments = mock(Instruments.class);
+    when(instruments.getPriceWidth()).thenReturn(10);
+    when(instruments.getSizeWidth()).thenReturn(10);
+    when(client.getInstruments()).thenReturn(instruments);
+    when(client.getEvents()).thenReturn(new Events());
+
+    // Act
+    tradesCommand.execute(client, new Scanner(""));
+
+    // Assert
+    verify(client).getInstruments();
+    verify(client).getEvents();
   }
 
   /**
