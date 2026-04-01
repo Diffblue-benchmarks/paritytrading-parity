87839b7551ed9232229b9d57655e56257cfef84e
Diffblue CI <ci@diffblue.com>
2026-04-01 02:50:18 +0000

    test(applications/client): Add generated tests for com.paritytrading.parity.client.ErrorsCommand.execute(Lcom/paritytrading/parity/client/TerminalClient;Ljava/util/Scanner;)V

--- applications/client/src/test/java/com/paritytrading/parity/client/ErrorsCommandDiffblueTest.java
+++ applications/client/src/test/java/com/paritytrading/parity/client/ErrorsCommandDiffblueTest.java
@@ -1,7 +1,10 @@
 package com.paritytrading.parity.client;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import com.diffblue.cover.annotations.ManagedByDiffblue;
 import com.diffblue.cover.annotations.MethodsUnderTest;
 import java.util.Scanner;
@@ -67,5 +70,30 @@ class ErrorsCommandDiffblueTest {
     assertEquals("Display occurred errors", actualDescription);
     assertEquals("errors", actualName);
     assertEquals("errors", actualErrorsCommand.getUsage());
+  }
+
+  /**
+   * Test {@link ErrorsCommand#execute(TerminalClient, Scanner)}.
+   *
+   * <ul>
+   *   <li>When {@link Scanner#Scanner(String)} with empty string.
+   *   <li>Then print errors header without exception.
+   * </ul>
+   *
+   * <p>Method under test: {@link ErrorsCommand#execute(TerminalClient, Scanner)}
+   */
+  @Test
+  @DisplayName(
+      "Test execute(TerminalClient, Scanner); when Scanner(String) with empty string; then print errors header")
+  @Tag("ContributionFromDiffblue")
+  @MethodsUnderTest({"void ErrorsCommand.execute(TerminalClient, Scanner)"})
+  void testExecute_whenScannerWithEmptyString_thenPrintErrorsHeader() {
+    // Arrange
+    ErrorsCommand errorsCommand = new ErrorsCommand();
+    TerminalClient mockClient = mock(TerminalClient.class);
+    when(mockClient.getEvents()).thenReturn(new Events());
+
+    // Act and Assert
+    assertDoesNotThrow(() -> errorsCommand.execute(mockClient, new Scanner("")));
   }
 }
