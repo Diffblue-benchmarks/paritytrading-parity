06ad148ad19fdfd6442a291bc3c7a5574cfb8c3c
Diffblue CI <ci@diffblue.com>
2026-04-01 02:21:44 +0000

    test(applications/reporter): Add generated tests for com.paritytrading.parity.reporter.TSVFormat
    - <init>
    - trade

--- applications/reporter/src/test/java/com/paritytrading/parity/reporter/TSVFormatDiffblueTest.java
+++ applications/reporter/src/test/java/com/paritytrading/parity/reporter/TSVFormatDiffblueTest.java
@@ -37,4 +37,88 @@ class TSVFormatDiffblueTest {
     // Assert
     verify(instruments).iterator();
   }
+
+  /**
+   * Test {@link TSVFormat#TSVFormat(Instruments)}.
+   *
+   * <ul>
+   *   <li>Given {@link Instruments} iterator returns an {@link Instrument}.
+   *   <li>Then calls {@link Instrument#getPriceFractionDigits()}.
+   * </ul>
+   *
+   * <p>Method under test: {@link TSVFormat#TSVFormat(Instruments)}
+   */
+  @Test
+  @DisplayName(
+      "Test new TSVFormat(Instruments); given Instruments iterator returns an Instrument; then calls getPriceFractionDigits()")
+  @Tag("ContributionFromDiffblue")
+  @ManagedByDiffblue
+  @MethodsUnderTest({"void TSVFormat.<init>(Instruments)"})
+  void testNewTSVFormat_givenInstrumentsIteratorReturnsInstrument_thenCallsGetPriceFractionDigits() {
+    // Arrange
+    Instrument instrument = mock(Instrument.class);
+    when(instrument.getPriceFractionDigits()).thenReturn(2);
+    when(instrument.getSizeFractionDigits()).thenReturn(2);
+    when(instrument.asString()).thenReturn("FOO");
+
+    ArrayList<Instrument> instrumentList = new ArrayList<>();
+    instrumentList.add(instrument);
+
+    Instruments instruments = mock(Instruments.class);
+    when(instruments.iterator()).thenReturn(instrumentList.iterator());
+
+    // Act
+    new TSVFormat(instruments);
+
+    // Assert
+    verify(instrument).getPriceFractionDigits();
+    verify(instrument).getSizeFractionDigits();
+    verify(instrument).asString();
+  }
+
+  /**
+   * Test {@link TSVFormat#trade(Trade)}.
+   *
+   * <ul>
+   *   <li>Given {@link Instrument} {@link Instrument#getPriceFactor()} return ten.
+   *   <li>Then calls {@link Instrument#getPriceFactor()}.
+   * </ul>
+   *
+   * <p>Method under test: {@link TSVFormat#trade(Trade)}
+   */
+  @Test
+  @DisplayName(
+      "Test trade(Trade); given Instrument getPriceFactor() return ten; then calls getPriceFactor()")
+  @Tag("ContributionFromDiffblue")
+  @ManagedByDiffblue
+  @MethodsUnderTest({"void TSVFormat.trade(Trade)"})
+  void testTrade_givenInstrumentGetPriceFactorReturnTen_thenCallsGetPriceFactor() {
+    // Arrange
+    Instrument instrument = mock(Instrument.class);
+    when(instrument.getPriceFactor()).thenReturn(10.0d);
+    when(instrument.getSizeFactor()).thenReturn(10.0d);
+    when(instrument.getPriceFractionDigits()).thenReturn(2);
+    when(instrument.getSizeFractionDigits()).thenReturn(2);
+    when(instrument.asString()).thenReturn("FOO");
+
+    ArrayList<Instrument> instrumentList = new ArrayList<>();
+    instrumentList.add(instrument);
+
+    Instruments instruments = mock(Instruments.class);
+    when(instruments.get(Mockito.<String>any())).thenReturn(instrument);
+    when(instruments.iterator()).thenReturn(instrumentList.iterator());
+
+    TSVFormat tsvFormat = new TSVFormat(instruments);
+
+    Trade event = new Trade();
+    event.instrument = "FOO";
+
+    // Act
+    tsvFormat.trade(event);
+
+    // Assert
+    verify(instrument).getPriceFactor();
+    verify(instrument).getSizeFactor();
+    verify(instruments).get("FOO");
+  }
 }
