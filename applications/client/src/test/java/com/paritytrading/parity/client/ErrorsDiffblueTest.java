19a3059e993e11313ba1092b75d5e78299327fc6
Diffblue CI <ci@diffblue.com>
2026-04-01 02:51:51 +0000

    test(applications/client): Add generated tests for com.paritytrading.parity.client.Errors.visit(Lcom/paritytrading/parity/client/Event/OrderRejected;)V

--- applications/client/src/test/java/com/paritytrading/parity/client/ErrorsDiffblueTest.java
+++ applications/client/src/test/java/com/paritytrading/parity/client/ErrorsDiffblueTest.java
@@ -1,8 +1,10 @@
 package com.paritytrading.parity.client;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.diffblue.cover.annotations.ManagedByDiffblue;
 import com.diffblue.cover.annotations.MethodsUnderTest;
+import com.paritytrading.parity.net.poe.POE;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -30,5 +32,32 @@ class ErrorsDiffblueTest {
 
     // Assert
     assertTrue(actualCollectResult.isEmpty());
+  }
+
+  /**
+   * Test {@link Errors#collect(Events)}.
+   *
+   * <ul>
+   *   <li>When {@link Events} has an {@link POE.OrderRejected} event.
+   *   <li>Then return list with one element.
+   * </ul>
+   *
+   * <p>Method under test: {@link Errors#collect(Events)}
+   */
+  @Test
+  @DisplayName("Test collect(Events); when Events has an OrderRejected event; then return list with one element")
+  @Tag("ContributionFromDiffblue")
+  @ManagedByDiffblue
+  @MethodsUnderTest({"List Errors.collect(Events)"})
+  void testCollect_whenEventsHasOrderRejected_thenReturnOneElement() {
+    // Arrange
+    Events events = new Events();
+    events.orderRejected(new POE.OrderRejected());
+
+    // Act
+    List<Error> actualCollectResult = Errors.collect(events);
+
+    // Assert
+    assertEquals(1, actualCollectResult.size());
   }
 }
