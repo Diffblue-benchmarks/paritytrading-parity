4657b67e67c7a7e6dc5e2b9567884e131fcb65c7
Diffblue CI <ci@diffblue.com>
2026-04-19 02:50:10 +0000

    test(libraries/net): Add generated tests for com.paritytrading.parity.net.poe.POEException.<init>(Ljava/lang/String;)V

---
+++ libraries/net/src/test/java/com/paritytrading/parity/net/poe/POEExceptionTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2014 Parity authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.paritytrading.parity.net.poe;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.paritytrading.parity.net.ProtocolException;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+class POEExceptionTest {
+
+    @Test
+    void constructWithMessage() {
+        POEException exception = new POEException("test error");
+
+        assertEquals("test error", exception.getMessage());
+    }
+
+    @Test
+    void isProtocolException() {
+        POEException exception = new POEException("protocol error");
+
+        assertTrue(exception instanceof ProtocolException);
+    }
+
+    @Test
+    void isIOException() {
+        POEException exception = new POEException("io error");
+
+        assertTrue(exception instanceof IOException);
+    }
+}
